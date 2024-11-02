fcd8d726ee040ae9d854cacbad7fe0d9b4a21851
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-11-02 15:06:56 +0000

    arreglado el orden de la luces en el intervalo

--- src/js/component/semafaro/Semafaro.jsx
+++ src/js/component/semafaro/Semafaro.jsx
@@ -1,67 +1,65 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import "./semafaro.css";
-const Semafaro = () => {
-    const [color, setColor] = useState("");
-    const [estilo, setEstilo] = useState("");
-    const [arrColor, setArrColor] = useState(["red", "yellow", "green"]);
-    
 
-    const addRed = () => {
-        setColor("red");
-    };
+  const Semafaro = () => {
+  const [color, setColor] = useState("");
+  const [estilo, setEstilo] = useState("");
+  const [arrColor,setArrColor] = useState(["green", "yellow", "red"]);
 
-    const addYellow = () => {
-        setColor("yellow");
-    };
-    const addGreen = () => {
-        setColor("green");
-    };
+  const addRed = () => {
+    setColor("red");
+  };
 
-    const addLightDiv = () => {
-        
-        setColor("purpura");
-        setEstilo("purpura light ");   
-    };
-   const removeLightDiv = () => {
-        setColor("");
-        setEstilo("");
-   }
-  
-   const intervalCambio =()=>{
-    
+  const addYellow = () => {
+    setColor("yellow");
+  };
+
+  const addGreen = () => {
+    setColor("green");
+  };
+
+  const addLightDiv = () => {
+    setColor("purpura");
+    setEstilo("purpura light");
+    setArrColor([...arrColor, "purpura"]);
+  };
+
+  const removeLightDiv = () => {
+    setColor("");
+    setEstilo("");
+  };
+
+  const intervalCambio = () => {
     arrColor.forEach((item, index) => {
-        setTimeout(() => {
-            setColor(item);
-        }, index * 2000);
-    })
-}
+      setTimeout(() => {
+        setColor(item);
+      }, index * 2000);
+    });
+  };
 
-    const turnON = () => {
-       
-           setInterval(() => {
-                intervalCambio();
-           }, 3000);
-}
-    return (
-        <>
-        <div className="container d-flex justify-content-center align-content-center w-100 mt-5">
-            <div className='trafficTop'></div>
-            <div className="semafaro ">
-                <div className={ color=="red"? "red light selected":"red light"} onClick={addRed}></div>
-                <div className={ color=="yellow"? "yellow light selected":"yellow light"} onClick={addYellow}></div>
-                <div className={color=="green"?  "green light selected":"green light"} onClick={addGreen}></div>
-                <div className={color=="purpura"? "purpura light selected ": estilo} onClick={addLightDiv}></div>
-            </div>
-            
+  const turnON = () => {
+    intervalCambio(); // 
+    setInterval(intervalCambio, arrColor.length * 2000); 
+  };
+
+  return (
+    <>
+      <div className="container d-flex justify-content-center align-content-center w-100 mt-5">
+        <div className='trafficTop'></div>
+        <div className="semafaro">
+          <div className={color === "red" ? "red light selected" : "red light"} onClick={addRed}></div>
+          <div className={color === "yellow" ? "yellow light selected" : "yellow light"} onClick={addYellow}></div>
+          <div className={color === "green" ? "green light selected" : "green light"} onClick={addGreen}></div>
+          <div className={color === "purpura" ? "purpura light selected" : estilo} onClick={addLightDiv}></div>
         </div>
-        <div className='d-flex justify-content-center gap-5'>
-            <button onClick={turnON} className="btn w-25 btn-primary mt-5">Encender</button>
-            <button onClick={removeLightDiv} className="btn w-25 btn-danger mt-5">Quitar Luz Purpura</button>
-            <button onClick={addLightDiv} className="btn w-25 btn-primary mt-5">Agregar Luz Purpura</button>
-       
-        </div>
-        </>
-    );
+      </div>
+      <div className='d-flex justify-content-center gap-5'>
+        <button onClick={turnON} className="btn w-25 btn-primary mt-5">Encender</button>
+        <button onClick={removeLightDiv} className="btn w-25 btn-danger mt-5">Quitar Luz Purpura</button>
+        <button onClick={addLightDiv} className="btn w-25 btn-primary mt-5">Agregar Luz Purpura</button>
+      </div>
+    </>
+  );
 };
 
 export default Semafaro;
